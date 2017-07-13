ba63e64150fde95fc3c2f3fc43c78289a6c20b25
wayhome25 <siwabada@gmail.com>
2017-07-13 23:56:55 +0900

    requirements.txt 최초 생성, 지역 및 언어설정 변경

--- our_book/config/settings.py
+++ our_book/config/settings.py
@@ -117,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
