f1df80331c399393ea1b0610179878bdbd76a5a6
wayhome25 <siwabada@gmail.com>
2017-07-14 10:04:21 +0900

    postgresql DB 설정 추가

--- our_book/config/settings.py
+++ our_book/config/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'our_book',
+        'USER': 'wayhome25',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
