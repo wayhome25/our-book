bc18b9a835d234dbd5c4134850295306549c2c2a
wayhome25 <siwabada@gmail.com>
2017-07-16 21:07:15 +0900

    context processor 생성 및 등록 - navbar 추가 및 로그인 폼 적용

--- our_book/config/settings.py
+++ our_book/config/settings.py
@@ -94,6 +94,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # Custom context processors
+                'accounts.context_processors.forms',
             ],
         },
     },
