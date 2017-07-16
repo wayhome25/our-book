c03f3e0e400669a01e7f2011a9dcf460dfe90823
wayhome25 <siwabada@gmail.com>
2017-07-16 13:26:54 +0900

    MEDIA 파일 서빙을 위한 설정 추가

--- our_book/config/urls.py
+++ our_book/config/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if 'proto' in settings.INSTALLED_APPS:
     urlpatterns += [
