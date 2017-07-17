ffe21690143c87336b8cf669323eb1eed072f9d9
wayhome25 <siwabada@gmail.com>
2017-07-17 14:27:14 +0900

    Book 모델 생성 및 기본정보 필드 추가

--- our_book/books/admin.py
+++ our_book/books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publisher', 'discount']
+    list_display_links = ['title', 'author', 'publisher']
+    search_fields = ['title']
+
--- our_book/books/models.py
+++ our_book/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    # 기본정보
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    isbn = models.CharField(max_length=15, unique=True)
+    image = models.URLField()
+    link = models.URLField()
+    price = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField()
+    pubdate = models.CharField(max_length=10)
