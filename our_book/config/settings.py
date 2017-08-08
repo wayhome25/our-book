f0bfb92fc7c480454f0a42961caee2ed51fc2e35
Lee HyunJoo <wayhome250@gmail.com>
2017-08-08 21:41:08 +0900

    ADD: i18n 다국어 지원을 위한 미들웨어 설정 추가

--- our_book/config/settings.py
+++ our_book/config/settings.py
@@ -76,6 +76,7 @@ AUTH_USER_MODEL = 'accounts.MyUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
